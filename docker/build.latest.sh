1342df71ebdcc6b0a1891d1ec45399e7d59820e4
yz181x <yz181x@gmail.com>
2024-02-24 13:23:38 +0800

    支持docker方式

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t yz181x/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag yz181x/chatgpt-on-wechat yz181x/chatgpt-on-wechat:$(date +%y%m%d)
